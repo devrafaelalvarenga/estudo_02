084476d42cc5eda51518b5bb0b0208b19058c3f6
rafael <devrafaelalvarenga@gmail.com>
2024-03-19 21:47:48 -0300

    add conversor_de_temperatura.py

---
+++ conversor_de_temperatura.py
@@ -0,0 +1,17 @@
+'''
+Escreva um programa que converta a temperatura de Celsius para Fahrenheit. 
+O programa deve solicitar ao usuário a temperatura em Celsius e, utilizando try-except, 
+garantir que a entrada seja numérica, tratando qualquer ValueError. 
+Imprima o resultado em Fahrenheit ou uma mensagem de erro se a entrada não for válida.
+'''
+
+celcius = float(input('Informe a temperatura em graus Celcius: '))
+try:
+    if isinstance(celcius, int) or isinstance(celcius, float):
+        fahrenheit = (celcius * 9/5) + 32
+        print(f'A conversão de {celcius}°C para fahrenheit é: {fahrenheit}°F ')
+except ValueError:
+    print('Por favor, digite um valor valido.')
+else:
+    print('Conversão efetuada com sucesso')    
+
