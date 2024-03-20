396a203e27e62891a62a3c57181fc8d8d9b0c172
rafael <devrafaelalvarenga@gmail.com>
2024-03-19 21:28:59 -0300

    update2 exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -70,7 +70,7 @@ print(f'A potencia de {num_1} elevado a {num_2} = {potencia}')
 '''
 celcius = float(input('Informe a temperatura em graus Celcius: '))
 fahrenheit = (celcius * 9/5) + 32
-print(f'A conversão de {celcius}°C para °F é: {fahrenheit} ')
+print(f'A conversão de {celcius}°C para fahrenheit é: {fahrenheit}°F ')
 '''
 
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
@@ -167,8 +167,61 @@ else:
 '''
 # #### try-except e if
 
-# 21: Conversor de Temperatura
-# 22: Verificador de Palíndromo
-# 23: Calculadora Simples
-# 24: Classificador de Números
-# 25: Conversão de Tipo com Validação
+#Exercício 21: Conversor de Temperatura
+#Escreva um programa que converta a temperatura de Celsius para Fahrenheit. O programa deve solicitar ao usuário a temperatura em Celsius e, utilizando try-except, garantir que a entrada seja numérica, tratando qualquer ValueError. Imprima o resultado em Fahrenheit ou uma mensagem de erro se a entrada não for válida.
+
+#Exercício 22: Verificador de Palíndromo
+#Crie um programa que verifica se uma palavra ou frase é um palíndromo (lê-se igualmente de trás para frente, desconsiderando espaços e pontuações). Utilize try-except para garantir que a entrada seja uma string. Dica: Utilize a função isinstance() para verificar o tipo da entrada.
+
+#Exercício 23: Calculadora Simples
+#Desenvolva uma calculadora simples que aceite duas entradas numéricas e um operador (+, -, *, /) do usuário. Use try-except para lidar com divisões por zero e entradas não numéricas. Utilize if-elif-else para realizar a operação matemática baseada no operador fornecido. Imprima o resultado ou uma mensagem de erro apropriada.
+
+#Exercício 24: Classificador de Números
+#Escreva um programa que solicite ao usuário para digitar um número. Utilize try-except para assegurar que a entrada seja numérica e utilize if-elif-else para classificar o número como "positivo", "negativo" ou "zero". Adicionalmente, identifique se o número é "par" ou "ímpar".
+
+#Exercício 25: Conversão de Tipo com Validação
+#Crie um script que solicite ao usuário uma lista de números separados por vírgula. O programa deve converter a string de entrada em uma lista de números inteiros. Utilize try-except para tratar a conversão de cada número e validar que cada elemento da lista convertida é um inteiro. Se a conversão falhar ou um elemento não for um inteiro, imprima uma mensagem de erro. Se a conversão for bem-sucedida para todos os elementos, imprima a lista de inteiros.
+
+'''
+# codigo sem erro
+
+nome = 'Rafael'
+try: #tenta isso se estiver correto imprimi o resultado
+    resultado = len(nome) #erro 
+    print(resultado)
+except: #se der errado imprimi o erro
+    print('Tudo errado')    
+else: #se der td certo, faça isso tb
+    print('Tudo certo!')
+finally: #independete do que aconteceu, faça isso tambem
+    print('Chegamos ao fim')     
+
+# codigo com erro
+
+nome = 'Rafael'
+try: #tenta isso se estiver correto imprimi o resultado
+    resultado = len(name) #erro 
+    print(resultado)
+except: #se der errado imprimi o erro
+    print('Tudo errado')    
+else: #se der td certo, faça isso tb
+    print('Tudo certo!')
+finally: #independete do que aconteceu, faça isso tambem
+    print('Chegamos ao fim')    
+'''
+
+'''
+# codigo sem erro
+nome = 'Rafael'
+if isinstance(nome, str): #verifique se a variavel é uma string
+    print('A variavel é uma string')
+else:
+    print('A variavel não é uma string')    
+
+# codigo sem erro
+nome = 123456
+if isinstance(nome, str): #verifique se a variavel é uma string
+    print('A variavel é uma string')
+else:
+    print('A variavel não é uma string')       
+'''    
